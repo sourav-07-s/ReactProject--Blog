22793d363a4d7503b3197bf63a2e39864ab33edd
sourav sahoo <souravsahoo407@gmail.com>
2026-09-20 00:57:10 +0530

    postform

--- Full-React-Project/src/components/index.js
+++ Full-React-Project/src/components/index.js
@@ -2,10 +2,11 @@ import {Header} from './Header/Header';
 import Footer from './Footer/Footer';
 import Container from './Container/Container';
 import LogoutBtn from './Header/LgoutBtn';
-
+import RTE from './RTE';
 export {
     Header,
     Footer,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
