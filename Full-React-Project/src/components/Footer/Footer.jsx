cd3c190f7b583b27850cd475322c46c949b1931b
sourav sahoo <souravsahoo407@gmail.com>
2026-09-24 02:40:39 +0530

    name change

--- Full-React-Project/src/components/Footer/Footer.jsx
+++ Full-React-Project/src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
               to="/"
               className="text-2xl font-bold"
             >
-              ShopAssist
+              Logo
             </Link>
           </div>
 
