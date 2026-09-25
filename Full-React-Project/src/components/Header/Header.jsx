a2fb0ad1d419dab2fcf8cc12c11e13dacf6f4759
sourav sahoo <souravsahoo407@gmail.com>
2026-09-26 01:06:18 +0530

    header modification

--- Full-React-Project/src/components/Header/Header.jsx
+++ Full-React-Project/src/components/Header/Header.jsx
@@ -36,7 +36,7 @@ export const Header = () => {
   ];
 
   return (
-    <header className="py-3 shadow bg-gray-800">
+    <header className="py-3 shadow bg-gray-800 border-amber-100 rounded-3xl w-80%">
       <Container>
         <nav className="flex items-center">
           <div className="mr-4">
